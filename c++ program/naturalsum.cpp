a12a091f33ab9239aa936c005986bd87a8ce7d91
Vikash Kaushik <vikashkaushik199@gmail.com>
2024-06-19 15:26:54 +0530

    n natural number program generated

---
+++ c++ program/naturalsum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int n, sum=0;
+   cout<<"Enter the natural number want to sum ";
+   cin>>n;
+   if(n<0){
+   cout<<"invalid";
+   }
+   else
+   cout<< n*(n+1)/2;
+
+
+   
+           return 0;
+   }
+
+   
+
+
+
